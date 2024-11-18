5b0407c4208c06144d0ba5d1d880c149e730718a
Vivek K J <vivekkj2004@gmail.com>
2024-11-18 20:17:33 +0530

    Bump deps. and fix CSP due to chrome 130 by upgrading @crxjs/vite-plugin to beta

--- src/manifest.js
+++ src/manifest.js
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   name: 'Linways Auto Feedback Filler',
   description: '',
-  version: '1.0.0',
+  version: '1.1.0',
   manifest_version: 3,
   icons: {
     16: 'img/logo-16.png',
